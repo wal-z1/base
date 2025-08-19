aa878221d9fe13e62f6dd7dc5f0204381dfc1653
Walid <ow_bouhenika@esi.dz>
2025-08-19 15:03:40 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<h1 class="text-3xl font-bold underline font-mozilla">Hello world!</h1>
+		</>
+	);
+}
+
+export default App;
